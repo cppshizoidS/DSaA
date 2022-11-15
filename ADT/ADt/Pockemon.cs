29c1d3321e77f8359bea0b46d8983f325448ddb9
Cppshizoid <alex.krivoschecov@gmail.com>
2022-11-14 16:36:09 -0800

    Create Pockemon.cs

---
+++ ADT/ADt/Pockemon.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AADS.ADT
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+        public int Exp { get; set; }
+
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
